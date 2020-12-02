81da296ef5937d75ea4fce5c7ec35e49a974fceb
SalahHW <salah.bouheniche@gmail.com>
2020-12-02 22:22:17 +0100

    Added the core fonction

--- 2_majuscule/majuscule.py
+++ 2_majuscule/majuscule.py
@@ -8,3 +8,21 @@ if len(sys.argv) < 2:
 elif len(sys.argv) > 2:
   print(filename+' require only one string as parameter.')
   quit()
+  
+def majuscule_minuscule(string):
+  new_string = ''
+  i = 0
+  for letter_index in range(len(string)):
+    if string[letter_index] == ' ':
+      new_string += ' '
+      continue
+    
+    if i % 2 == 0:
+      new_string += string[letter_index].lower()
+      i += 1
+    else:
+      new_string += string[letter_index].upper()
+      i += 1
+  return new_string
+      
+print(majuscule_minuscule(sys.argv[1]))
